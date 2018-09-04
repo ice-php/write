59119a0f8184dfdedbbbd2ab42c8f4d49aeed59d
lanbing <liyong@nenu.edu.cn>
2018-09-04 16:33:16 +0800

    写文件,解决Linux下的权限及用户问题

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
-# write
-处理Linux下的写权限和用户问题
+# 处理Linux下的写权限和用户问题
+
+## 对file_put_contents的封装,以修正文件所有者
+write(string $file, $content, int $flag = 0)
+
+## 越级创建目录
+makeDir(string $path): void
+
--- src/write.php
+++ src/write.php
@@ -3,6 +3,13 @@ declare(strict_types=1);
 
 namespace icePHP;
 
+/**
+ * 对file_put_contents的封装,以修正文件所有者
+ * @param $file string 文件名
+ * @param $content string 文件内容
+ * @param int $flag FILE_APPEND&|LOCK_EX
+ * @return false|int
+ */
 function write(string $file, $content, int $flag = 0)
 {
     //当前用户
@@ -36,4 +43,47 @@ function write(string $file, $content, int $flag = 0)
     }
 
     return $ret;
+}
+
+/**
+ * 越级创建目录
+ * @param $path string 目录名称
+ */
+function makeDir(string $path): void
+{
+    //转换标准路径 分隔符
+    $path = str_replace(['\\', '/'], DIRECTORY_SEPARATOR, $path);
+
+    //如果已经是目录或文件
+    if (is_dir($path) or is_file($path)) return;
+
+    //上一级目录
+    $parent = dirname($path);
+
+    //如果上一级不是目录,则创建上一级
+    if (!is_dir($parent)) {
+        makeDir($parent);
+    }
+
+    //创建当前目录
+    mkdir($path, 0777);
+
+    //Windows系统,不进行后续处理
+    if (isWindows()) {
+        return;
+    }
+
+    //当前用户
+    $current = getenv('USERNAME') ?: getenv('USER');
+
+    //应该是这个用户
+    $should = Config::get('system', 'OS_USER');
+
+    //如果当前已经是应该的用户,则不处理
+    if ($current === $should) {
+        return;
+    }
+
+    //修改所有者为www(应该的用户)
+    chown($path, $should);
 }
